88d0963f4110963d428cefbfb38600ec31ababdc
Facundo <salasfacundo96@gmail.com>
2024-10-01 15:21:18 -0300

    work experience is loaded from json

--- src/app/dashboard/components/work-experience/work-experience.component.ts
+++ src/app/dashboard/components/work-experience/work-experience.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkExperience } from '../../models/work-experience';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'work-experience',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.scss'
+  styleUrl: './work-experience.component.scss',
+  providers:[HttpClient]
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements OnInit {
 
+  works: WorkExperience[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(){
+    this.httpClient.get<{ works: WorkExperience[] }>('assets/data/workExperience.json')
+    .subscribe(response => {
+      this.works = response.works;
+    });
+  }
 }
---
+++ src/app/dashboard/models/work-experience.ts
@@ -0,0 +1,7 @@
+export interface WorkExperience{
+  role: string,
+  company: string,
+  startDate: string,
+  endDate: string | null,
+  description: string
+}
